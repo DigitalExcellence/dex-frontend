f1ce58eb62c777b66d72e80412e4914a48942183
Joris Coenen <joris_coenen-@hotmail.com>
2020-03-24 10:48:57 +0100

    Added html with demo data in overview-project

--- src/app/modules/project/overview/overview.component.ts
+++ src/app/modules/project/overview/overview.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/models/domain/project';
+import { ProjectService } from 'src/app/services/project.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { User } from 'src/app/models/domain/user';
 
 @Component({
   selector: 'app-overview',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OverviewComponent implements OnInit {
 
-  constructor() { }
+  public projects: Project[];
+
+  constructor(private projectService: ProjectService) { }
 
   ngOnInit(): void {
+    this.projects = [];
+    const tempUser: User = {
+      id: 1,
+      profileUrl: '',
+      username: 'Kasper Hämäläinen'
+    };
+
+    const project: Project = {
+      id: 0,
+      name: 'FEEL THE SPARK - GLOW 2018',
+      shortDescription: 'Etiam rhoncus maecenas tempus tellus eget condimentum rhoncus.',
+      description: 'Etiam rhoncus maecenas tempus tellus eget condimentum rhoncus.',
+      createdDate: new Date('2018-11-20'),
+      url: '',
+      user: tempUser,
+      contributors: null
+    };
+    for (let index = 0; index < 10; index++) {
+      project.id++;
+      this.projects.push(project);
+    }
+
+    // this.projectService.getAll().subscribe(result => {
+    //   this.projects = result;
+    // }, (error: HttpErrorResponse) => {
+    //   if (error.status !== 404) {
+    //     console.log('Could not retrieve the projects');
+    //   }
+    // });
   }
 
 }
