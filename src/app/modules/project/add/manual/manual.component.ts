806040a83cde630c3603713d7f903678a44d7df9
Ruben Brugman <r.brugman@student.fontys.nl>
2020-03-27 16:49:19 +0100

    Implemented AddProject component

--- src/app/modules/project/add/manual/manual.component.ts
+++ src/app/modules/project/add/manual/manual.component.ts
@@ -1,6 +1,12 @@
+import { ProjectService } from 'src/app/services/project.service';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { ProjectAdd } from 'src/app/models/resources/project-add';
+import { CollaboratorAdd } from 'src/app/models/resources/contributor-add';
 
+/**
+ * Component for manually adding a project.
+ */
 @Component({
   selector: 'app-manual',
   templateUrl: './manual.component.html',
@@ -8,27 +14,75 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class ManualComponent implements OnInit {
 
+  /**
+   * Formgroup for entering project details.
+   */
   public newProjectForm: FormGroup;
+  public newContributorForm: FormGroup;
 
-  private contributors: string[];
+  /**
+   * Project's contributors.
+   */
+  public collaborators: CollaboratorAdd[] = [];
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private projectService: ProjectService) {
+
     this.newProjectForm = this.formBuilder.group({
       name: [null, Validators.required],
       uri: [null, Validators.required],
       shortDescription: [null, Validators.required],
       description: [null, null]
     });
+
+    this.newContributorForm = this.formBuilder.group({
+      fullName: [null, Validators.required],
+      role: [null, Validators.required]
+    });
+
   }
 
   ngOnInit(): void {
-  }
-
-  public onSubmit() {
 
   }
 
-  public onAddContributor() {
-    
+  public onSubmit(): void {
+    if (!this.newProjectForm.valid) {
+      // Todo display error.
+      return;
+    }
+
+    const newProject: ProjectAdd = this.newProjectForm.value;
+    newProject.collaborators = this.collaborators;
+    // Todo implement service call.
+  }
+
+  /**
+   * Method which triggers when the add contributor button is pressed.
+   * Adds submitted contributor to the contributors array.
+   */
+  public onClickAddContributor(): void {
+    if (!this.newContributorForm.valid) {
+      // Todo display error.
+      return;
+    }
+
+    const newContributor: CollaboratorAdd = this.newContributorForm.value;
+    this.collaborators.push(newContributor);
+    this.newContributorForm.reset();
+  }
+
+  /**
+   * Method which triggers when the delete contributor button is pressed.
+   * Removes the contributors from the contributors array.
+   */
+  public onClickDeleteContributor(clickedContributor: CollaboratorAdd): void {
+    const index = this.collaborators.findIndex(contributor => contributor === clickedContributor);
+    if (index < 0) {
+      // Todo display error.
+      return;
+    }
+    this.collaborators.splice(index, 1);
   }
 }
