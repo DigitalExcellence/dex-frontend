052a7f787b433605f392c28c3cf13830286a61e2
Brend Smits <brend_smits@hotmail.com>
2020-04-11 16:42:37 +0200

    Added GNU Lesser General Public License v3.0

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,20 @@
+/* 
+ *  Digital Excellence Copyright (C) 2020 Brend Smits
+ *   
+ *   This program is free software: you can redistribute it and/or modify 
+ *   it under the terms of the GNU Lesser General Public License as published 
+ *   by the Free Software Foundation version 3 of the License.
+ *   
+ *   This program is distributed in the hope that it will be useful, 
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty 
+ *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *   See the GNU Lesser General Public License for more details.
+ *   
+ *   You can find a copy of the GNU Lesser General Public License 
+ *   along with this program, in the LICENSE.md file in the root project directory.
+ *   If not, see https://www.gnu.org/licenses/lgpl-3.0.txt 
+ */
+
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
