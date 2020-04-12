273b45fc29b580878a1cd6837942b86f3352897d
Elaine <e.heijboer@student.fontys.nl>
2020-04-12 17:34:04 +0200

    styling search bar home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './modules/home/home.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/project/overview', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'project', loadChildren: () => import('./modules/project/project.module').then(m => m.ProjectModule) }
 ];
 
