afc146c0f816372b22d1eced9b1329093d3d98ff
Rens <rens.nooren@student.fontys.nl>
2020-04-16 11:16:24 +0200

    Basic overview styling html

--- src/app/modules/project/project.module.ts
+++ src/app/modules/project/project.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { OverviewComponent } from './overview/overview.component';
 import { ProjectRoutingModule } from './project-routing.module';
 import { DetailsComponent } from './details/details.component';
+import {ClrAccordionModule} from '@clr/angular';
 
 @NgModule({
   declarations: [OverviewComponent, DetailsComponent],
   imports: [
-    CommonModule,
-    ProjectRoutingModule
-  ]
+        CommonModule,
+        ProjectRoutingModule,
+        ClrAccordionModule
+    ]
 })
 export class ProjectModule { }
