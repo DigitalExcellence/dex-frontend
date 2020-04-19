ba775f86d65c2a57566ac8d180f3f92bbdc82132
Brend Smits <brend_smits@hotmail.com>
2020-04-19 20:04:57 +0200

    Added License to new files

--- src/app/components/auth-callback/auth-callback.component.ts
+++ src/app/components/auth-callback/auth-callback.component.ts
@@ -1,3 +1,20 @@
+/*
+ *  Digital Excellence Copyright (C) 2020 Brend Smits
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published
+ *   by the Free Software Foundation version 3 of the License.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty
+ *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   See the GNU Lesser General Public License for more details.
+ *
+ *   You can find a copy of the GNU Lesser General Public License
+ *   along with this program, in the LICENSE.md file in the root project directory.
+ *   If not, see https://www.gnu.org/licenses/lgpl-3.0.txt
+ */
+
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { AuthService } from "src/app/services/auth.service";
@@ -5,7 +22,7 @@ import { AuthService } from "src/app/services/auth.service";
 @Component({
   selector: "app-auth-callback",
   templateUrl: "./auth-callback.component.html",
-  styleUrls: ["./auth-callback.component.css"],
+  styleUrls: ["./auth-callback.component.scss"],
 })
 export class AuthCallbackComponent implements OnInit {
   public error: boolean;
