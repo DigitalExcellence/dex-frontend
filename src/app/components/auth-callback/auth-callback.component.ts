0fc91977c4d0e0ec8bb06871234ea82d8ae59ba9
Brend Smits <brend_smits@hotmail.com>
2020-04-19 13:16:53 +0200

    Remove async in ngOnInit for auth-callback. async here will be very difficult to debug in the future if anything ever goes wrong for whatever reason.

--- src/app/components/auth-callback/auth-callback.component.ts
+++ src/app/components/auth-callback/auth-callback.component.ts
@@ -12,14 +12,14 @@ export class AuthCallbackComponent implements OnInit {
 
   constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute) {}
 
-  async ngOnInit() {
+  ngOnInit() {
     // check for error
     if (this.route.snapshot.fragment.indexOf("error") >= 0) {
       this.error = true;
       return;
     }
 
-    await this.authService.completeAuthentication();
+    this.authService.completeAuthentication();
     this.router.navigate(["/home"]);
   }
 }
