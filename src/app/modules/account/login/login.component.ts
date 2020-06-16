3be073d82666074ac4ccfd5aa291a323993655c2
Rene <r.vanvliet@student.fontys.nl>
2020-06-16 20:20:18 +0200

    added a privacy modal

--- src/app/modules/account/login/login.component.ts
+++ src/app/modules/account/login/login.component.ts
@@ -17,6 +17,10 @@
 
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
+import { ModalAcceptGenericComponent } from 'src/app/components/modals/modal-accept-generic/modal-accept-generic.component';
+import { Observable, EMPTY } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -24,16 +28,46 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,
+              private modalService: BsModalService,
+    ) { }
   public readonly title = 'Sign in';
 
   /**
    * Method which triggers when the login FHICT button is pressed.
    */
   public onClickLoginFHICT() {
-    this.authService.login('FHICT');
+    this.loginWithConsent('FHICT');
   }
   public onClickLoginIdentityServer() {
-    this.authService.login();
+    this.loginWithConsent();
+  }
+
+  /**
+   * Checks if the user already has given consent to use their data.
+   * If the user did not consent before a modal is shown to request consent.
+   */
+  private loginWithConsent(provider?: string): void {
+    const privacyConsentKeyName = 'PrivacyConsentGiven';
+    if (localStorage.getItem(privacyConsentKeyName) === 'true') {
+      this.authService.login(provider);
+    } else {
+      // set modal options
+      const modalOptions: ModalOptions = {
+        initialState: {
+          titleText: 'We value your privacy!',
+          mainText: `By clicking accept, you agree to our <a href="/policy" target="_blank">Privacy Policy</a>. You acknowledge that you have read and understand it. If you have questions or concerns you can contact us.`,
+        }
+      };
+      // Display modal
+      const modalRef = this.modalService.show(ModalAcceptGenericComponent, modalOptions);
+      // wait for the true emitter
+      modalRef.content.accept.subscribe((result: boolean) => {
+        if (result) {
+          localStorage.setItem(privacyConsentKeyName, 'true');
+          this.authService.login(provider);
+        }
+      });
+    }
   }
 }
