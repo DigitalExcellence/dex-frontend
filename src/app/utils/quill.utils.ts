1633149fe35cfd55b519d594d74e8af99e76fca3
Ruben <r.brugman@student.fontys.nl>
2020-06-19 12:00:58 +0200

    Change toolbar options & removed hover override
    Hover override seemed unfeasible due to the svg structure

--- src/app/utils/quill.utils.ts
+++ src/app/utils/quill.utils.ts
@@ -40,7 +40,7 @@ export class QuillUtils {
                 [{ 'color': [] }, { 'background': [] }],
                 [{ 'align': [] }],
                 ['clean'],
-                ['link', 'image', 'video']
+                ['link']
             ],
             markdownShortcuts: {}
         };
