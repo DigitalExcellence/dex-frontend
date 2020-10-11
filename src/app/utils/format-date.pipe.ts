a568b8762ce0257648aece8c36b0d34a5a08f23e
Martin Markov <martin.markov28@gmail.com>
2020-10-11 18:13:42 +0200

    Added edit highlight functionality

---
+++ src/app/utils/format-date.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
+
+@Pipe({
+  name: 'formatDate'
+})
+export class FormatDatePipe implements PipeTransform {
+  public transform(date: string): string {
+    return date ? moment(date).format('dddd, DD-MM-YYYY') : 'Never ending';
+  }
+}
