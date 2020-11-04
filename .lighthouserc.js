6ec5183e186c27051e972cc18c93970d998dbda5
Niek van Dam <niekvandam1@outlook.com>
2020-11-04 14:44:22 +0100

    updated CI paramters

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
     ci: {
      // Static site example
         collect: {
-            startServerCommand: 'npm run npm start',
+            startServerCommand: 'npm run -start',
             url: ['http://localhost:4200'],
             numberOfRuns: 9
         },
