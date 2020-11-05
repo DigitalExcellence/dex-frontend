60c6d8ee094aaaee49768e6e40ee4954994c830f
Ruben <rubenfricke@gmail.com>
2020-11-05 09:13:33 +0100

    Created basic Call to action service

---
+++ src/app/services/call-to-action.service.ts
@@ -0,0 +1,33 @@
+/*
+ *  Digital Excellence Copyright (C) 2020 Brend Smits
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published
+ *   by the Free Software Foundation version 3 of the License.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty
+ *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   See the GNU Lesser General Public License for more details.
+ *
+ *   You can find a copy of the GNU Lesser General Public License
+ *   along with this program, in the LICENSE.md file in the root project directory.
+ *   If not, see https://www.gnu.org/licenses/lgpl-3.0.txt
+ */
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { API_CONFIG } from '../config/api-config';
+import { CallToAction } from '../models/domain/call-to-action';
+import { CallToActionAdd } from '../models/resources/calltoaction-add';
+import { CallToActionUpdate } from '../models/resources/calltoaction-update';
+import { HttpBaseService } from './http-base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CallToActionService extends HttpBaseService<CallToAction, CallToActionAdd, CallToActionUpdate> {
+    constructor(http: HttpClient) {
+      super(http, API_CONFIG.url + API_CONFIG.callToActionRoute);
+    }
+  }
