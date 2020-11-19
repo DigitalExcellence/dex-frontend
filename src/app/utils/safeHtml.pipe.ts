212b93b8d4e7acc5ce320db1ffe476a4e4c4aa9b
Spacinggolem <waltersajtos@outlook.com>
2020-11-19 13:29:54 +0100

    Removed bypassSecurityTrustHtml from the safeHtml pipe

--- src/app/utils/safeHtml.pipe.ts
+++ src/app/utils/safeHtml.pipe.ts
@@ -5,6 +5,6 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class SafeHtmlPipe implements PipeTransform  {
   constructor(private sanitized: DomSanitizer) {}
   transform(value) {
-    return this.sanitized.bypassSecurityTrustHtml(value);
+    return this.sanitized.sanitize(1, value);
   }
 }
