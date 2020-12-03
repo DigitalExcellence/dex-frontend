1d94d47fd7690be83a00143b3a7a8fa56bc1e058
Spacinggolem <waltersajtos@outlook.com>
2020-12-03 09:03:21 +0100

    Added documentation to collaborators

--- src/app/modules/project/collaborator/collaborator.component.ts
+++ src/app/modules/project/collaborator/collaborator.component.ts
@@ -16,10 +16,18 @@ export class CollaboratorComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  /**
+   * Method that is called when a user clicks the follow button on a collaborator
+   * This functionality is not implemented YET
+   */
   public onFollowClick(projectId: number) {
-
+    // TODO: Implement this function when following users is added to the backend
   }
 
+  /**
+   * Check whether the the application is running in production mode
+   * So we can hide the items that are not implemented yet
+   */
   public isProduction(): boolean {
     return !environment.production;
   }
