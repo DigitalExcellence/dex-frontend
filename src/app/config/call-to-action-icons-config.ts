b82b160dfdc87a78cb2f8ed417dd77f83a511261
Spacinggolem <waltersajtos@outlook.com>
2021-04-10 22:55:53 +0200

    Created config file for icon names

---
+++ src/app/config/call-to-action-icons-config.ts
@@ -0,0 +1,17 @@
+export interface CallToActionIconsConfig {
+  1: string; // Join today
+  2: string; // Provide feedback
+  3: string; // Apply now
+  4: string; // Collaborate with us
+  5: string; // More information
+  6: string; // Get in touch
+}
+
+export const CallToActionIconsConfig: CallToActionIconsConfig = {
+  1: 'fa-user-check',
+  2: 'fa-comments',
+  3: 'fa-plus',
+  4: 'fa-users',
+  5: 'fa-info-circle',
+  6: 'fa-hand-pointer'
+};
