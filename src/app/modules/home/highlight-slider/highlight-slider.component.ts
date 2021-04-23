c7be0e5dda35983f10b7f63a42cd701cfd723025
macfleury-2000 <f.vanosta@student.fontys.nl>
2021-04-23 10:49:43 +0200

    BGimg to header and the buttons work now in header

--- src/app/modules/home/highlight-slider/highlight-slider.component.ts
+++ src/app/modules/home/highlight-slider/highlight-slider.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-highlight-slider',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HighlightSliderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    
+  }
+
+  public viewAllProjects() {
+    if (this.router.url === '/project/overview') {
+      location.reload();
+    } else {
+      this.router.navigate(['project/overview']);
+    }
+  }
+
+  public viewAddProject() {
+    if (this.router.url === '/project/add/source') {
+      location.reload();
+    } else {
+      this.router.navigate(['project/add/source']);
+    }
   }
 
 }
