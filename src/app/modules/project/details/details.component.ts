3b2bdddd32b12b3756dfe5b4879818b2e24bf0a9
Exosfeer <andrewnoorden@hotmail.com>
2021-04-30 14:28:38 +0200

    :sparkles: test data
    :sparkles: test data

--- src/app/modules/project/details/details.component.ts
+++ src/app/modules/project/details/details.component.ts
@@ -72,6 +72,12 @@ export class DetailsComponent implements OnInit {
   public displayCallToActionButton = true;
   public displayHighlightButton = false;
   public displayEmbedButton = false;
+  public projectComments = [
+    {id: 1, text: 'Sentence 1'},
+    {id: 2, text: 'Sentence 2'},
+    {id: 3, text: 'Sentence 3'},
+    {id: 4, text: 'Sentenc4 '},
+  ];
 
   /**
    * Property to indicate whether the project is loading.
