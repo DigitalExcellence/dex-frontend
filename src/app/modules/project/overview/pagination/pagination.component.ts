fb9a3bd9ce33c74b34ba9a0049aa0134c1f8aa5b
Spacinggolem <waltersajtos@outlook.com>
2021-07-25 15:46:52 +0200

    Fix sonar cloud issues

--- src/app/modules/project/overview/pagination/pagination.component.ts
+++ src/app/modules/project/overview/pagination/pagination.component.ts
@@ -1,20 +1,15 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss']
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent {
   @Input() amountOfProjectsOnSinglePage: number;
   @Input() totalNumberOfProjects: number;
-  @Input() showPaginationFooter: Boolean;
+  @Input() showPaginationFooter: boolean;
 
   @Output() pageChanged = new EventEmitter<number>();
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
