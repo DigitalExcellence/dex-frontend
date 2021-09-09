72d93e44340c11726281b58d4d69e14764257759
Sven Hansen <svenroermond@hotmail.com>
2021-09-09 14:31:37 +0200

    Updated Imports

--- src/app/modules/project/details/bottom-drawer/bottom-drawer.component.ts
+++ src/app/modules/project/details/bottom-drawer/bottom-drawer.component.ts
@@ -1,16 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
-
+import { Project } from 'src/app/models/domain/project';
+import { scopes } from 'src/app/models/domain/scopes';
+import { User } from 'src/app/models/domain/user';
+import { AlertConfig } from 'src/app/models/internal/alert-config';
+import { AlertType } from 'src/app/models/internal/alert-type';
 import { AlertService } from 'src/app/services/alert.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { HighlightByProjectIdService } from 'src/app/services/highlightid.service';
 
-import { scopes } from 'src/app/models/domain/scopes';
-
-import { AlertType } from 'src/app/models/internal/alert-type';
-import { AlertConfig } from 'src/app/models/internal/alert-config';
-
-import { Project } from 'src/app/models/domain/project';
-import { User } from 'src/app/models/domain/user';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bottom-drawer',
