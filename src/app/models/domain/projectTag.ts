b03b7e31e52a1ef741f5173aa296a0fd4c66ba25
Julia <71012148+juliaslaats@users.noreply.github.com>
2021-12-16 09:50:46 +0100

    added tags to proejct interface

--- src/app/models/domain/projectTag.ts
+++ src/app/models/domain/projectTag.ts
@@ -15,30 +15,7 @@
  *   If not, see https://www.gnu.org/licenses/lgpl-3.0.txt
  */
 
-import { CallToAction } from './call-to-action';
-import { Collaborator } from './collaborator';
-import { ProjectCategory } from './projectCategory';
-import { ProjectLike } from './projectLike';
-import { ProjectTag } from './projectTag';
-import { UploadFile } from './uploadFile';
-import { User } from './user';
-
-export interface Project {
+export interface ProjectTag {
   id: number;
-  user?: User;
   name: string;
-  description?: string;
-  shortDescription?: string;
-  created: Date;
-  updated: Date;
-  uri?: string;
-  collaborators?: Collaborator[];
-  callToActions?: CallToAction[];
-  projectIcon?: UploadFile;
-  images?: UploadFile[];
-  likes?: Array<ProjectLike>;
-  userHasLikedProject: boolean;
-  likeCount: number;
-  categories?: Array<ProjectCategory>;
-  tags?: Array<ProjectTag>;
 }
